921c5fa38a956fae235c0a8f0951676d4cacb057
zt1020 <58399089+zt1020@users.noreply.github.com>
2020-11-11 15:50:54 -0500

    added deckofcards and a main method to test

---
+++ src/goFish/Main.java
@@ -0,0 +1,11 @@
+package goFish;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DeckOfCards deck = new DeckOfCards();
+		deck.generate();
+	}
+
+}
